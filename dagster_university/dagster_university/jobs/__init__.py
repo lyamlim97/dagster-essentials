4faa21141fe4a3f637c575223c74cc82dc7a3fa2
lyamlim97 <lyamlim97@gmail.com>
2024-12-15 14:56:13 +0800

    Add job

---
+++ dagster_university/dagster_university/jobs/__init__.py
@@ -0,0 +1,5 @@
+from dagster import AssetSelection, define_asset_job
+
+trips_by_week = AssetSelection.assets("trips_by_week")
+
+trip_update_job = define_asset_job(name="trip_update_job", selection=AssetSelection.all() - trips_by_week)
